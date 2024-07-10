73a3680506ce09d93ca27eaeba0fb385734a9a42
Vivek Madhukar <vivek.madhukar@timesinternet.in>
2024-07-10 23:37:31 +0530

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by Vivek Madhukar on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Appetizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Appetizer
+//
+//  Created by Vivek Madhukar on 10/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
